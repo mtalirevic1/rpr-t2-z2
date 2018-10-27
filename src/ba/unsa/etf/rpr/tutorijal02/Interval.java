0c6e58926688c72325570ab25fde8fb56fcb5f3e
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2018-10-27 10:37:49 +0200

    Metode prolaze sve testove.

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -9,42 +9,42 @@ public class Interval {
 
     public Interval(double pocetnaTacka,double krajnjaTacka, boolean imaPocetna, boolean imaKrajnja){
         if(pocetnaTacka>krajnjaTacka) throw new IllegalArgumentException("Interval nije validan");
-        this.pocetnaTacka=pocetnaTacka;
-        this.krajnjaTacka=krajnjaTacka;
-        this.imaPocetna=imaPocetna;
-        this.imaKrajnja=imaKrajnja;
+        this.setPocetnaTacka(pocetnaTacka);
+        this.setKrajnjaTacka(krajnjaTacka);
+        this.setImaPocetna(imaPocetna);
+        this.setImaKrajnja(imaKrajnja);
 
     }
 
     public Interval(){
-        this.pocetnaTacka=0;
-        this.krajnjaTacka=0;
-        this.imaPocetna=false;
-        this.imaKrajnja=false;
+        this.setPocetnaTacka(0);
+        this.setKrajnjaTacka(0);
+        this.setImaPocetna(false);
+        this.setImaKrajnja(false);
 
     }
 
     public boolean isIn(double v) {
-        if(imaPocetna==true && imaKrajnja==true){
-            if(v>=pocetnaTacka && v<=krajnjaTacka)
+        if(isImaPocetna() ==true && isImaKrajnja() ==true){
+            if(v>= getPocetnaTacka() && v<= getKrajnjaTacka())
                 return true;
             else
                 return false;
         }
-        else if(imaPocetna==false && imaKrajnja==true) {
-            if (v > pocetnaTacka && v <= krajnjaTacka)
+        else if(isImaPocetna() ==false && isImaKrajnja() ==true) {
+            if (v > getPocetnaTacka() && v <= getKrajnjaTacka())
                 return true;
             else
                 return false;
         }
-        else if(imaPocetna==false && imaKrajnja==false) {
-            if (v > pocetnaTacka && v < krajnjaTacka)
+        else if(isImaPocetna() ==false && isImaKrajnja() ==false) {
+            if (v > getPocetnaTacka() && v < getKrajnjaTacka())
                 return true;
             else
                 return false;
         }
-        else if(imaPocetna==true && imaKrajnja==false) {
-            if (v >= pocetnaTacka && v < krajnjaTacka)
+        else if(isImaPocetna() ==true && isImaKrajnja() ==false) {
+            if (v >= getPocetnaTacka() && v < getKrajnjaTacka())
                 return true;
             else
                 return false;
@@ -53,7 +53,7 @@ public class Interval {
     }
 
     public boolean isNull() {
-        if(this.pocetnaTacka==0 && this.krajnjaTacka==0 && this.imaPocetna==false && this.imaKrajnja==false)
+        if(this.getPocetnaTacka() ==0 && this.getKrajnjaTacka() ==0 && this.isImaPocetna() ==false && this.isImaKrajnja() ==false)
             return true;
         else
             return false;
@@ -61,21 +61,75 @@ public class Interval {
 
 
     public static Interval intersect(Interval i1, Interval i2){
-        if((i1.krajnjaTacka<i2.pocetnaTacka && i1.pocetnaTacka<i2.pocetnaTacka) || (i2.krajnjaTacka<i1.pocetnaTacka && i2.pocetnaTacka<i1.pocetnaTacka))
+        if((i1.getKrajnjaTacka() < i2.getPocetnaTacka() && i1.getPocetnaTacka() < i2.getPocetnaTacka()) || (i2.getKrajnjaTacka() < i1.getPocetnaTacka() && i2.getPocetnaTacka() < i1.getPocetnaTacka()))
             return new Interval();
-        else if((i1.krajnjaTacka<i2.krajnjaTacka && i1.pocetnaTacka>i2.pocetnaTacka))
-            return new Interval(i1.pocetnaTacka,i1.krajnjaTacka,i1.imaPocetna,i1.imaKrajnja);
-        else if((i2.krajnjaTacka<i1.krajnjaTacka && i2.pocetnaTacka>i1.pocetnaTacka))
-            return new Interval(i2.pocetnaTacka,i2.krajnjaTacka,i2.imaPocetna,i2.imaKrajnja);
-        else if(i1.pocetnaTacka<i2.pocetnaTacka && i1.krajnjaTacka>i2.krajnjaTacka)
-            return new Interval(i2.pocetnaTacka,i1.krajnjaTacka,i2.imaPocetna,i1.imaKrajnja);
-        else if(i2.pocetnaTacka<i1.pocetnaTacka && i2.krajnjaTacka>i1.krajnjaTacka)
-            return new Interval(i1.pocetnaTacka,i2.krajnjaTacka,i1.imaPocetna,i2.imaKrajnja);
-        else if(i1.pocetnaTacka==i2.pocetnaTacka && i1.krajnjaTacka==i2.krajnjaTacka)
-            return new Interval(i1.pocetnaTacka,i1.krajnjaTacka,i1.imaPocetna || i2.imaPocetna,i1.imaKrajnja || i2.imaKrajnja);
+        if((i1.getKrajnjaTacka() < i2.getKrajnjaTacka() && i1.getPocetnaTacka() > i2.getPocetnaTacka()))
+            return new Interval(i1.getPocetnaTacka(), i1.getKrajnjaTacka(), i1.isImaPocetna(), i1.isImaKrajnja());
+        if((i2.getKrajnjaTacka() < i1.getKrajnjaTacka() && i2.getPocetnaTacka() > i1.getPocetnaTacka()))
+            return new Interval(i2.getPocetnaTacka(), i2.getKrajnjaTacka(), i2.isImaPocetna(), i2.isImaKrajnja());
+        if(i1.getPocetnaTacka() < i2.getPocetnaTacka() && i1.getKrajnjaTacka() < i2.getKrajnjaTacka())
+            return new Interval(i2.getPocetnaTacka(), i1.getKrajnjaTacka(), i2.isImaPocetna(), i1.isImaKrajnja());
+        if(i2.getPocetnaTacka() < i1.getPocetnaTacka() && i2.getKrajnjaTacka() < i1.getKrajnjaTacka())
+            return new Interval(i1.getPocetnaTacka(), i2.getKrajnjaTacka(), i1.isImaPocetna(), i2.isImaKrajnja());
+        if(i1.getPocetnaTacka() == i2.getPocetnaTacka() && i1.getKrajnjaTacka() == i2.getKrajnjaTacka())
+            return new Interval(i1.getPocetnaTacka(), i1.getKrajnjaTacka(), i1.isImaPocetna() || i2.isImaPocetna(), i1.isImaKrajnja() || i2.isImaKrajnja());
+        return new Interval(5,5,false,false);
     }
 
     public Interval intersect(Interval i){
-        return intersect(*this,i);
+        return intersect(this,i);
+    }
+
+    public String toString(){
+        if(isNull()==true)
+            return "()";
+        else if(this.isImaPocetna()==true && this.isImaKrajnja()==true)
+            return "["+getPocetnaTacka()+","+getKrajnjaTacka()+"]";
+        else if(this.isImaPocetna()==false && this.isImaKrajnja()==true)
+            return "("+getPocetnaTacka()+","+getKrajnjaTacka()+"]";
+        else if(this.isImaPocetna()==false && this.isImaKrajnja()==false)
+            return "("+getPocetnaTacka()+","+getKrajnjaTacka()+")";
+        else if(this.isImaPocetna()==true && this.isImaKrajnja()==false)
+            return "["+getPocetnaTacka()+","+getKrajnjaTacka()+")";
+        return "";
+    }
+
+    public boolean equals(Interval i){
+        if(this.getPocetnaTacka()==i.getPocetnaTacka() && this.getKrajnjaTacka()==i.getKrajnjaTacka() && this.isImaPocetna()==i.isImaPocetna() && this.isImaKrajnja()==i.isImaKrajnja())
+            return true;
+        else
+            return false;
+    }
+
+    public double getPocetnaTacka() {
+        return pocetnaTacka;
+    }
+
+    public void setPocetnaTacka(double pocetnaTacka) {
+        this.pocetnaTacka = pocetnaTacka;
+    }
+
+    public double getKrajnjaTacka() {
+        return krajnjaTacka;
+    }
+
+    public void setKrajnjaTacka(double krajnjaTacka) {
+        this.krajnjaTacka = krajnjaTacka;
+    }
+
+    public boolean isImaPocetna() {
+        return imaPocetna;
+    }
+
+    public void setImaPocetna(boolean imaPocetna) {
+        this.imaPocetna = imaPocetna;
+    }
+
+    public boolean isImaKrajnja() {
+        return imaKrajnja;
+    }
+
+    public void setImaKrajnja(boolean imaKrajnja) {
+        this.imaKrajnja = imaKrajnja;
     }
 }
